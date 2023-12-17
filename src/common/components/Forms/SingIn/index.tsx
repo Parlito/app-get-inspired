86dddf1b8f25d7b6e448968e918f975ba71beb52
Paulo Henrique <contatopauloeditor@gmail.com>
2023-12-17 16:08:40 -0300

    Criação do formulário de login

---
+++ src/common/components/Forms/SingIn/index.tsx
@@ -0,0 +1,39 @@
+import React,{useState} from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Footer, Form, Title } from './styled'
+import Input from '../../Controllers/Input'
+import Button from '../../Controllers/Button'
+import FooterButton from '../../Controllers/FooterButton'
+
+function SingIn() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    setIsLoading(true);
+  }
+
+  function handleForgotPassword() {
+
+  }
+  
+return (
+    <Form>
+        <Title>Entrar</Title>
+        <Input placeholder='E-mail' onChangeText={setEmail}/>
+        <Input placeholder='Senha' secureTextEntry onChangeText={setPassword}/>
+        <Button title='Entrar' onPress={handleSignIn} isLoading={isLoading}/>
+        
+        <Footer>
+            <FooterButton title='Criar conta' icon="person-add" onPress={() => navigation.navigate('home')}/>
+            <FooterButton title='Esqueci a minha senha' icon="email" onPress={handleForgotPassword}/>
+        </Footer>
+    </Form>
+  )
+}
+
+export default SingIn
