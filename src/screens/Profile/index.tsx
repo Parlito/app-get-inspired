1d31cf39d39703395038ab7792e819c201370e0d
Paulo Henrique <contatopauloeditor@gmail.com>
2023-12-17 15:38:57 -0300

    Teste do botão

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Title } from './styled'
+import Button from '../../common/components/Controllers/Button'
+import { Alert } from 'react-native';
+
 
 function Profile() {
+
+  const [isLoading, setIsLoading] = useState();
+
+  function handleNewAccount() {
+    Alert.alert("Olá");
+  }
+
   return (
     <Container>
         <Title>
             Perfil
         </Title>
+        <Button title='Cadastrar' isLoading={isLoading} onPress={handleNewAccount}/>
     </Container>
   )
 }
