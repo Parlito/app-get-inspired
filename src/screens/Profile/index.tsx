e5cf45516d970026fcaa0cbff74398a229400986
Paulo Henrique <contatopauloeditor@gmail.com>
2023-12-17 15:46:02 -0300

    Finalização dos testes

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,25 +1,15 @@
 import React, { useState } from 'react'
 import { Container, Title } from './styled'
-import Button from '../../common/components/Controllers/Button'
-import { Alert } from 'react-native';
-import Input from '../../common/components/Controllers/Input';
+
 
 
 function Profile() {
-  const [email, setEmail] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  function handleNewAccount() {
-    Alert.alert("Olá");
-  }
 
   return (
     <Container>
         <Title>
             Perfil
         </Title>
-        <Input/>
-        <Button title='Cadastrar' isLoading={isLoading} onPress={handleNewAccount}/>
     </Container>
   )
 }
