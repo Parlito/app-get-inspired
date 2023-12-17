249dbfb291a3994988bc9df50da69ef5e974afe2
Paulo Henrique <contatopauloeditor@gmail.com>
2023-12-17 15:32:06 -0300

    Criação do componente do botão

---
+++ src/common/components/Controllers/Button/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Load, Title } from './styled'
+import { RectButtonProps } from 'react-native-gesture-handler';
+
+type Props = RectButtonProps & {
+    title: string;
+    isLoading?: boolean;
+  };
+
+function Button({ title, isLoading = false, ...rest} : Props) {
+  return (
+    <Container enabled = {isLoading} {...rest}>
+        {isLoading ? <Load/> : <Title>{title}</Title>}
+    </Container>
+  )
+}
+
+export default Button
