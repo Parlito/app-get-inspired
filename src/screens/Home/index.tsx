def155068e01154708994a729c043a506b5719d2
Paulo Henrique <contatopauloeditor@gmail.com>
2023-12-20 14:58:51 -0300

    Retirada do Header do projeto

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,11 @@
 import React, {useState} from 'react'
 import { Container, Title } from './styled'
-import { Header } from '../../common/components/Layout/Header'
 
 
 function Home() {
   return (
     <Container>
-        <Header/>
+
     </Container>
   )
 }
