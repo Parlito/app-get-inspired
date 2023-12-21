9f2967cbd5e872b5421d287498e4f3b643aa1dd7
Paulo Henrique <contatopauloeditor@gmail.com>
2023-12-21 11:33:24 -0300

    Melhorando o design do Drawer, colocando foto e nome do perfil do usuário

--- src/common/components/Layout/Drawer/CustomDrawer.tsx
+++ src/common/components/Layout/Drawer/CustomDrawer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Alert, View } from 'react-native'
+import { Alert, ImageBackground, View, Image, Text } from 'react-native'
 import { DrawerContentScrollView, DrawerItemList  } from '@react-navigation/drawer'
 import Button from '../../Controllers/Button'
 import { LogoutButton } from '../../Controllers/LogoutButton'
@@ -32,9 +32,23 @@ const CustomDrawer = props => {
 
   return (
         <View style={{flex: 1}}>
-        <DrawerContentScrollView {...props}>
-            <DrawerItemList {...props}/>
+        <DrawerContentScrollView 
+        {...props}
+        contentContainerStyle={{backgroundColor: '#39cff2'}}
+        >
+        <ImageBackground 
+        source={{uri:'https://i.pinimg.com/736x/9b/b2/f9/9bb2f90b3cf1595681c6ac21a536d221.jpg',}}
+        style={{padding:20}}>
+            <Image 
+               source={{uri:'https://th.bing.com/th/id/R.19fa7497013a87bd77f7adb96beaf768?rik=144XvMigWWj2bw&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f5%2fUser-Profile-PNG-High-Quality-Image.png&ehk=%2bat%2brmqQuJrWL609bAlrUPYgzj%2b%2f7L1ErXRTN6ZyxR0%3d&risl=&pid=ImgRaw&r=0',}}
+               style={{height:80, width: 80, borderRadius:40, marginBottom:10}}/>
+            <Text style={{color:'#318ce7', fontSize: 18, marginBottom:5, fontWeight:'bold', marginStart:10}}>Usuário</Text>
+        </ImageBackground>
+        <View style={{flex:1,backgroundColor:'white',paddingTop:20}}>   
+        <DrawerItemList {...props}/>  
+        </View>
         </DrawerContentScrollView>
+        
         {user ? <View style={{padding:20, borderTopWidth: 1, borderTopColor: '#ccc'}}>
             
             <FooterButton title='Sair da conta' icon="logout" onPress={handleLogOut}/> 
