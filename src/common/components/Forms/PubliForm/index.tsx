c2eb08ce99f95c68454de1d53d60f4e338f5fc5d
Paulo Henrique <contatopauloeditor@gmail.com>
2023-12-23 11:58:41 -0300

    mudando algumas questões de publicação e da página Home

--- src/common/components/Forms/PubliForm/index.tsx
+++ src/common/components/Forms/PubliForm/index.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, NormalText, Title, User } from './styled'
+import Input from '../../Controllers/Input';
+import Button from '../../Controllers/Button';
 
 interface PostProps{
   item: {
-    id: string;
     user: string;
     title: string;
     body: string;
   };
+  id: string;
 }
 
-export const PubliForm = ({item} : PostProps) => {
-  return (
-    <Container activeOpacity={0.9}>
-        <Title>{item.title}</Title>
-        <User>{item.user}</User>
-        <NormalText>{item.body.substring(0, 255) + '...'}</NormalText>
+export const PubliForm = ({item, id} : PostProps) => {
 
+  const [titulo, setTitulo] = useState('');
+  const [description, setDescription] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleNewPubli() {
+    setIsLoading(true);
+  }
+
+  return (
+    <Container>
+      <Title>Nova publicação</Title>
+      <Input placeholder='Título da publicação' onChangeText={setTitulo}/>
+      <Input placeholder='Sua história' onChangeText={setDescription}/>
+
+      <Button title='Publicar!' isLoading={isLoading} onPress={handleNewPubli}/>
     </Container>
   );
 };
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,49 +1,15 @@
-import React, {useState} from 'react'
-import { Container, Title } from './styled'
+import React, {useEffect, useState} from 'react'
 import Button from '../../common/components/Controllers/Button'
 import { PubliForm } from '../../common/components/Forms/PubliForm'
 import { ScrollView, FlatList } from 'react-native';
-
+import { Container, Title } from './styled';
 
 function Home() {
+
   return (
-    <>
-      <ScrollView style={{flex: 1, paddingTop: 40,}} 
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{
-        alignItems: 'center',
-        paddingBottom: 100,
-      }}
-      nestedScrollEnabled>
-      <Button title='Publicar'/>
-      <ScrollView
-          contentContainerStyle={{
-            width: '100%',
-          }}
-          horizontal
-          scrollEnabled={false}
-        >
-          <FlatList
-            data={[
-              {
-                id: 'react-native-evolution',
-                user: 'João',
-                title: 'React Native está cada vez mais evoluido',
-                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-              },
-              {
-                id: 'react-native-evolution-part-two',
-                user: 'João',
-                title: 'React Native está cada vez mais evoluido - parte 2',
-                body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-              },
-            ]}
-            renderItem={({ item, index }) => <PubliForm item={item} />}
-            keyExtractor={(item, index) => String(index)}
-          />
-        </ScrollView>
-      </ScrollView>
-      </>
+   <Container>
+    <Title>Home</Title>
+   </Container>
   );
 }
 
